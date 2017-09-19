559e05c7cf4f24f9c20c496bde9cae076a91add5
Bijesh Kawan <kawanbijesh@gmail.com>
2017-09-19 18:21:35 +0545

    working with php_object data types

--- data_types/objects_data.php
+++ data_types/objects_data.php
@@ -1,7 +1,65 @@
 <html>
 <body>
 <?php
-
+class smartP{
+    function smartP(){
+        $this->model = iPhone;
+    }
+}
+$mobile = new smartP();
+echo $mobile->model;
 ?>
 </body>
 </html>
+
+
+
+
+<!---->
+<!--<html>-->
+<!--<body>-->
+<?php
+//class smartP{
+//    function test(){
+//        $this->model = iPhone;
+//    }
+//}
+//$mobile = new smartP();
+//$mobile->test();
+//echo $mobile->model;
+//?>
+<!--</body>-->
+<!--</html>-->
+
+
+<!---->
+<!--<html>-->
+<!--<body>-->
+<?php
+//class smartP{
+//    function smartP(){
+//        $this->model = iPhone;
+//    }
+//}
+//$mobile = new smartP();
+//echo $mobile->model;
+//?>
+<!--</body>-->
+<!--</html>-->
+
+<!---->
+<!---->
+<!--<html>-->
+<!--<body>-->
+<?php
+//class smartP{
+//    function smartP(){
+//        $this->model ;
+//    }
+//}
+//$mobile = new smartP();
+//echo $mobile->model =samsung;
+//?>
+<!--</body>-->
+<!--</html>-->
+<!---->
